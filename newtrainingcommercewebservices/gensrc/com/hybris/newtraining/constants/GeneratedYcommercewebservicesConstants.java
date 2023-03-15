2b25f006dbc2ec6d12d91376d161665754660775
Nagolu <bindu.nagolu@capgemini.com>
2023-03-15 12:38:53 +0530

    newtrainingcommercewebservices

---
+++ newtrainingcommercewebservices/gensrc/com/hybris/newtraining/constants/GeneratedYcommercewebservicesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14-Mar-2023, 7:17:22 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.newtraining.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYcommercewebservicesConstants
+{
+	public static final String EXTENSIONNAME = "newtrainingcommercewebservices";
+	
+	protected GeneratedYcommercewebservicesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ newtrainingcommercewebservices/src/com/hybris/newtraining/queues/impl/ProductExpressUpdateQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.hybris.newtraining.queues.impl;
+
+import com.hybris.newtraining.queues.data.ProductExpressUpdateElementData;
+
+
+/**
+ * Queue for {@link com.hybris.newtraining.queues.data.ProductExpressUpdateElementData}
+ */
+public class ProductExpressUpdateQueue extends AbstractUpdateQueue<ProductExpressUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
